8509270511fbadc1ee9d10cb77469af090224e61
OxanaTs <kseniats08@gmail.com>
2024-04-04 20:37:41 +0200

    project finished

--- main.js
+++ main.js
@@ -1,38 +1,70 @@
 const prevBtn = document.querySelector('#prev')
 const nextBtn = document.querySelector('#next')
-const title = document.querySelector('.post h1')
-const text = document.querySelector('.post p')
+const titleBox = document.querySelector('.post h1')
+const textBox = document.querySelector('.post p')
+const postBox = document.querySelector('.post')
 
 const API_URL = 'https://jsonplaceholder.typicode.com'
+let currentPostId = 1
 
-const getPostdetails = async (id) => {
+const errorBox = (message) => {
+    const errorBox = document.createElement('div')
+    errorBox.className = 'error'
+    errorBox.textContent = err.message
+    return errorBox
+}
+
+const createLoader = () => {
+    const loader = document.createElement('div')
+    const loaderIcon = document.createElement('i')
+    loader.className = 'loader'
+    loaderIcon.className = 'bx bx-loader-alt loader-icon'
+    loader.append(loaderIcon)
+    return loader
+}
+
+const preloadPost = () => {
+    const loader = createLoader()
+    postBox.append(loader)
+    document.querySelector('.post .error')?.remove()
+    return loader
+}
+
+const getPostDetails = async (id) => {
+    const loader = preloadPost()
+    try {
     const response = await fetch(`${API_URL}/posts/${id}`)
+    if (!response.ok) throw new Error('Failed to download')
      const data = await response.json()
      return data
+    } catch (err) {
+    console.log(err)
+    const errorBox = createErrorBox(err.message)
+    document.querySelector('.post').append(errorBox)
+    }
+    finally {
+        loader.remove()
+    }
+}
+
+const renderPost = async (id) => {
+    const { title, body } =  await getPostDetails(id)
+    titleBox.textContent = title
+    textBox.textContent = body
 }
 
 prevBtn.addEventListener('click', async () => {
-    if (postNum > 1) {
-        postNum++
-        switchToPost()
+    currentPostId--
+    if (currentPostId <= 1) {
+        prevBtn.disabled = true
     }
-
+    renderPost(currentPostId)
 })
-nextBtn.addEventListener('click', () => {
-    postNum++
-    switchToPost()
+nextBtn.addEventListener('click', async () => {
+    currentPostId++
+    if (currentPostId > 1) prevBtn.disabled = false
+    renderPost(currentPostId)
 })
-async function switchToPost() {
-    try {
-        const postText = await getPostdetails(postNum)
-        title.textContent = postText.title
-        text.textContent = postText.body
-    } catch (error) {
-        console.error('Post Load Failed', error)
-        title.textContent = 'Error'
-        text.textContent = 'Failed'
-    }
-}
 
-let postNum = 1 
-loadPost()
+renderPost(1)
+
